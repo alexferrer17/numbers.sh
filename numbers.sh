996165b5e6af8506fa286a4901687aec508532f6
alexferrer17 <36723243+alexferrer17@users.noreply.github.com>
2019-04-22 16:54:21 -0700

    Update numbers.sh

--- numbers.sh
+++ numbers.sh
@@ -3,7 +3,15 @@
 #Alejandro Ferrer-Peasley
 echo "enter a positive number: "
 read NUMBER
-while 
-do
+COUNTER=0
+         while [  $COUNTER -lt NUMBER]
+         do
+          if [ $((COUNTER%2)) -eq 0]
+          then
+             echo $COUNTER is even
+          else
+             echo $COUNTER is odd
+          fi
+         done
 
 ~   
